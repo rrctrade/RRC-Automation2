653e4b645a754403adcbf6ca89c54747c2b05c0e
rrctrade <prasadpokam50@gmail.com>
2026-01-22 09:26:28 +0530

    Update sector_mapping.py

--- sector_mapping.py
+++ sector_mapping.py
@@ -8,21 +8,13 @@ SECTOR_MAP = {
 
     # ===================== NIFTY AUTO ======================
     "AUTO": [
-        "NSE:UNOMINDA-EQ",
-        "NSE:TMPV-EQ",
-        "NSE:ASHOKLEY-EQ",
-        "NSE:BAJAJ-AUTO-EQ",
-        "NSE:MARUTI-EQ",
-        "NSE:EICHERMOT-EQ",
-        "NSE:TVSMOTOR-EQ",
-        "NSE:BHARATFORG-EQ",
+       
         "NSE:EXIDEIND-EQ",
         "NSE:BOSCHLTD-EQ",
         "NSE:M&M-EQ",
-        "NSE:MOTHERSON-EQ",
         "NSE:SONACOMS-EQ",
         "NSE:HEROMOTOCO-EQ",
-        "NSE:TIINDIA-EQ"
+        
     ],
 
     # ================= NIFTY FINANCIAL SERVICES =================
@@ -30,23 +22,7 @@ SECTOR_MAP = {
         "NSE:SHRIRAMFIN-EQ",
         "NSE:BSE-EQ",
         "NSE:PFC-EQ",
-        "NSE:RECLTD-EQ",
-        "NSE:ICICIBANK-EQ",
-        "NSE:JIOFIN-EQ",
-        "NSE:BAJAJFINSV-EQ",
-        "NSE:LICHSGFIN-EQ",
-        "NSE:AXISBANK-EQ",
-        "NSE:HDFCBANK-EQ",
-        "NSE:ICICIPRULI-EQ",
-        "NSE:SBICARD-EQ",
-        "NSE:MUTHOOTFIN-EQ",
-        "NSE:BAJFINANCE-EQ",
-        "NSE:ICICIGI-EQ",
-        "NSE:SBILIFE-EQ",
-        "NSE:KOTAKBANK-EQ",
-        "NSE:SBIN-EQ",
-        "NSE:HDFCLIFE-EQ",
-        "NSE:CHOLAFIN-EQ"
+        
     ],
 
     # ===================== NIFTY FMCG ======================
@@ -55,17 +31,7 @@ SECTOR_MAP = {
         "NSE:RADICO-EQ",
         "NSE:UNITDSPR-EQ",
         "NSE:UBL-EQ",
-        "NSE:NESTLEIND-EQ",
-        "NSE:EMAMILTD-EQ",
-        "NSE:ITC-EQ",
-        "NSE:HINDUNILVR-EQ",
-        "NSE:DABUR-EQ",
-        "NSE:GODREJCP-EQ",
-        "NSE:COLPAL-EQ",
-        "NSE:BRITANNIA-EQ",
-        "NSE:MARICO-EQ",
-        "NSE:TATACONSUM-EQ",
-        "NSE:PATANJALI-EQ"
+        
     ],
 
     # ===================== NIFTY IT ======================
@@ -73,13 +39,7 @@ SECTOR_MAP = {
         "NSE:PERSISTENT-EQ",
         "NSE:WIPRO-EQ",
         "NSE:INFY-EQ",
-        "NSE:TECHM-EQ",
-        "NSE:HCLTECH-EQ",
-        "NSE:TCS-EQ",
-        "NSE:COFORGE-EQ",
-        "NSE:OFSS-EQ",
-        "NSE:LTIM-EQ",
-        "NSE:MPHASIS-EQ"
+        
     ],
 
     # ===================== NIFTY MEDIA ======================
@@ -87,13 +47,7 @@ SECTOR_MAP = {
         "NSE:SUNTV-EQ",
         "NSE:ZEEL-EQ",
         "NSE:NETWORK18-EQ",
-        "NSE:HATHWAY-EQ",
-        "NSE:PFOCUS-EQ",
-        "NSE:NAZARA-EQ",
-        "NSE:DBCORP-EQ",
-        "NSE:SAREGAMA-EQ",
-        "NSE:TIPSMUSIC-EQ",
-        "NSE:PVRINOX-EQ"
+       
     ],
 
     # ===================== NIFTY METAL ======================
@@ -101,18 +55,7 @@ SECTOR_MAP = {
         "NSE:NATIONALUM-EQ",
         "NSE:LLOYDSME-EQ",
         "NSE:HINDCOPPER-EQ",
-        "NSE:WELCORP-EQ",
-        "NSE:SAIL-EQ",
-        "NSE:NMDC-EQ",
-        "NSE:HINDZINC-EQ",
-        "NSE:APLAPOLLO-EQ",
-        "NSE:JSWSTEEL-EQ",
-        "NSE:HINDALCO-EQ",
-        "NSE:ADANIENT-EQ",
-        "NSE:JINDALSTEL-EQ",
-        "NSE:VEDL-EQ",
-        "NSE:TATASTEEL-EQ",
-        "NSE:JSL-EQ"
+        
     ],
 
     # ===================== NIFTY PHARMA ======================
@@ -120,23 +63,7 @@ SECTOR_MAP = {
         "NSE:PPLPHARMA-EQ",
         "NSE:WOCKPHARMA-EQ",
         "NSE:AJANTPHARM-EQ",
-        "NSE:GLENMARK-EQ",
-        "NSE:LAURUSLABS-EQ",
-        "NSE:SUNPHARMA-EQ",
-        "NSE:ALKEM-EQ",
-        "NSE:GLAND-EQ",
-        "NSE:MANKIND-EQ",
-        "NSE:IPCALAB-EQ",
-        "NSE:DIVISLAB-EQ",
-        "NSE:DRREDDY-EQ",
-        "NSE:LUPIN-EQ",
-        "NSE:TORNTPHARM-EQ",
-        "NSE:BIOCON-EQ",
-        "NSE:JBCHEPHARM-EQ",
-        "NSE:AUROPHARMA-EQ",
-        "NSE:ZYDUSLIFE-EQ",
-        "NSE:ABBOTINDIA-EQ",
-        "NSE:CIPLA-EQ"
+        
     ],
 
     # ===================== NIFTY PSU BANK ======================
@@ -144,15 +71,7 @@ SECTOR_MAP = {
         "NSE:PSB-EQ",
         "NSE:PNB-EQ",
         "NSE:UCOBANK-EQ",
-        "NSE:CENTRALBK-EQ",
-        "NSE:CANBK-EQ",
-        "NSE:INDIANB-EQ",
-        "NSE:MAHABANK-EQ",
-        "NSE:BANKBARODA-EQ",
-        "NSE:IOB-EQ",
-        "NSE:BANKINDIA-EQ",
-        "NSE:SBIN-EQ",
-        "NSE:UNIONBANK-EQ"
+       
     ],
 
     # ===================== NIFTY PRIVATE BANK ======================
@@ -160,13 +79,7 @@ SECTOR_MAP = {
         "NSE:INDUSINDBK-EQ",
         "NSE:RBLBANK-EQ",
         "NSE:ICICIBANK-EQ",
-        "NSE:IDFCFIRSTB-EQ",
-        "NSE:FEDERALBNK-EQ",
-        "NSE:YESBANK-EQ",
-        "NSE:AXISBANK-EQ",
-        "NSE:HDFCBANK-EQ",
-        "NSE:KOTAKBANK-EQ",
-        "NSE:BANDHANBNK-EQ"
+        
     ],
 
     # ===================== NIFTY REALTY ======================
@@ -174,13 +87,8 @@ SECTOR_MAP = {
         "NSE:ANANTRAJ-EQ",
         "NSE:SOBHA-EQ",
         "NSE:PHOENIXLTD-EQ",
-        "NSE:BRIGADE-EQ",
-        "NSE:LODHA-EQ",
-        "NSE:DLF-EQ",
-        "NSE:OBEROIRLTY-EQ",
-        "NSE:SIGNATURE-EQ",
-        "NSE:GODREJPROP-EQ",
-        "NSE:PRESTIGE-EQ"
+        
+       
     ],
 
     # ===================== NIFTY CONSUMER DURABLES ======================
@@ -188,18 +96,7 @@ SECTOR_MAP = {
         "NSE:WHIRLPOOL-EQ",
         "NSE:CROMPTON-EQ",
         "NSE:VGUARD-EQ",
-        "NSE:HAVELLS-EQ",
-        "NSE:CERA-EQ",
-        "NSE:CENTURYPLY-EQ",
-        "NSE:VOLTAS-EQ",
-        "NSE:AMBER-EQ",
-        "NSE:KALYANKJIL-EQ",
-        "NSE:TITAN-EQ",
-        "NSE:BATAINDIA-EQ",
-        "NSE:KAJARIACER-EQ",
-        "NSE:PGEL-EQ",
-        "NSE:BLUESTARCO-EQ",
-        "NSE:DIXON-EQ"
+    
     ],
 
     # ===================== NIFTY OIL & GAS ======================
@@ -207,16 +104,7 @@ SECTOR_MAP = {
         "NSE:GSPL-EQ",
         "NSE:BPCL-EQ",
         "NSE:GAIL-EQ",
-        "NSE:HINDPETRO-EQ",
-        "NSE:MGL-EQ",
-        "NSE:PETRONET-EQ",
-        "NSE:CASTROLIND-EQ",
-        "NSE:RELIANCE-EQ",
-        "NSE:ONGC-EQ",
-        "NSE:IGL-EQ",
-        "NSE:IOC-EQ",
-        "NSE:GUJGASLTD-EQ",
-        "NSE:OIL-EQ"
+      
     ],
 
     # ===================== NIFTY FIN SERVICES EX BANK ======================
@@ -225,32 +113,8 @@ SECTOR_MAP = {
         "NSE:SHRIRAMFIN-EQ",
         "NSE:BSE-EQ",
         "NSE:IRFC-EQ",
-        "NSE:ANGELONE-EQ",
-        "NSE:360ONE-EQ",
-        "NSE:PFC-EQ",
-        "NSE:CDSL-EQ",
-        "NSE:RECLTD-EQ",
-        "NSE:LTF-EQ",
-        "NSE:JIOFIN-EQ",
-        "NSE:PNBHOUSING-EQ",
-        "NSE:MFSL-EQ",
-        "NSE:CAMS-EQ",
-        "NSE:BAJAJFINSV-EQ",
-        "NSE:LICI-EQ",
-        "NSE:LICHSGFIN-EQ",
-        "NSE:ICICIPRULI-EQ",
-        "NSE:SBICARD-EQ",
-        "NSE:MUTHOOTFIN-EQ",
-        "NSE:BAJFINANCE-EQ",
-        "NSE:ICICIGI-EQ",
-        "NSE:SBILIFE-EQ",
-        "NSE:IEX-EQ",
-        "NSE:HDFCAMC-EQ",
-        "NSE:POLICYBZR-EQ",
-        "NSE:PAYTM-EQ",
-        "NSE:ABCAPITAL-EQ",
-        "NSE:HDFCLIFE-EQ",
-        "NSE:CHOLAFIN-EQ"
+      
+       
     ],
 
     # ===================== NIFTY CHEMICALS ======================
@@ -259,39 +123,13 @@ SECTOR_MAP = {
         "NSE:CHAMBLFERT-EQ",
         "NSE:UPL-EQ",
         "NSE:SUMICHEM-EQ",
-        "NSE:DEEPAKFERT-EQ",
-        "NSE:BAYERCROP-EQ",
-        "NSE:AARTIIND-EQ",
-        "NSE:HSCL-EQ",
-        "NSE:TATACHEM-EQ",
-        "NSE:DEEPAKNTR-EQ",
-        "NSE:NAVINFLUOR-EQ",
-        "NSE:COROMANDEL-EQ",
-        "NSE:PIIND-EQ",
-        "NSE:PCBL-EQ",
-        "NSE:LINDEINDIA-EQ",
-        "NSE:PIDILITIND-EQ",
-        "NSE:FLUOROCHEM-EQ",
-        "NSE:SRF-EQ"
+       
     ],
 
     # ===================== NIFTY 50 ======================
     "NIFTY50": [
         "NSE:TRENT-EQ","NSE:SHRIRAMFIN-EQ","NSE:WIPRO-EQ","NSE:INFY-EQ",
-        "NSE:BHARTIARTL-EQ","NSE:TECHM-EQ","NSE:TMPV-EQ","NSE:BAJAJ-AUTO-EQ",
-        "NSE:MARUTI-EQ","NSE:SUNPHARMA-EQ","NSE:BEL-EQ","NSE:HCLTECH-EQ",
-        "NSE:JSWSTEEL-EQ","NSE:HINDALCO-EQ","NSE:TCS-EQ","NSE:EICHERMOT-EQ",
-        "NSE:ICICIBANK-EQ","NSE:ADANIENT-EQ","NSE:NESTLEIND-EQ",
-        "NSE:RELIANCE-EQ","NSE:JIOFIN-EQ","NSE:ADANIPORTS-EQ",
-        "NSE:ONGC-EQ","NSE:APOLLOHOSP-EQ","NSE:POWERGRID-EQ","NSE:ITC-EQ",
-        "NSE:ETERNAL-EQ","NSE:BAJAJFINSV-EQ","NSE:DRREDDY-EQ",
-        "NSE:COALINDIA-EQ","NSE:NTPC-EQ","NSE:ULTRACEMCO-EQ",
-        "NSE:AXISBANK-EQ","NSE:M&M-EQ","NSE:HINDUNILVR-EQ",
-        "NSE:TATASTEEL-EQ","NSE:HDFCBANK-EQ","NSE:ASIANPAINT-EQ",
-        "NSE:LT-EQ","NSE:BAJFINANCE-EQ","NSE:TITAN-EQ","NSE:MAXHEALTH-EQ",
-        "NSE:SBILIFE-EQ","NSE:INDIGO-EQ","NSE:GRASIM-EQ","NSE:CIPLA-EQ",
-        "NSE:KOTAKBANK-EQ","NSE:SBIN-EQ","NSE:TATACONSUM-EQ",
-        "NSE:HDFCLIFE-EQ"
+       
     ],
 
     # ===================== NIFTY BANK ======================
@@ -299,14 +137,6 @@ SECTOR_MAP = {
         "NSE:PNB-EQ",
         "NSE:INDUSINDBK-EQ",
         "NSE:ICICIBANK-EQ",
-        "NSE:CANBK-EQ",
-        "NSE:IDFCFIRSTB-EQ",
-        "NSE:FEDERALBNK-EQ",
-        "NSE:BANKBARODA-EQ",
-        "NSE:AXISBANK-EQ",
-        "NSE:HDFCBANK-EQ",
-        "NSE:AUBANK-EQ",
-        "NSE:KOTAKBANK-EQ",
-        "NSE:SBIN-EQ"
+       
     ]
 }
